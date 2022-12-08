b58b091c16e420538e05b17b82be781e3cc73dfe
denghao <1051766345@qq.com>
2022-12-08 23:59:30 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false,
-  devServer:{
-    open:true,
-    host:'localhost',
-    port:8080,
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 8080,
     // 代理跨域
-    proxy:{
-      '/api':{
-        target:"http://gmall-h5-api.atguigu.cn"
+    proxy: {
+      '/api': {
+        target: "http://gmall-h5-api.atguigu.cn"
       }
     },
   },
-  publicPath: './',
+  publicPath: process.env.node_env === "production" ? "./" : "/"
 })
